a32a2184c0faffd61882c1e507a1008db0a7ba52
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-03-31 22:11:55 +0530

    implement the list todo and styling

--- ToDo/client/src/App.jsx
+++ ToDo/client/src/App.jsx
@@ -7,7 +7,6 @@ function App() {
   return (
     <>
       <InputTodo />
-      <EditTodo />
       <ListTodo />
     </>
   );
--- ToDo/client/src/components/ListTodo.jsx
+++ ToDo/client/src/components/ListTodo.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const ListTodo = () => {
-  return (
-    <div>ListTodo</div>
-  )
-}
+  const [todos, setTodos] = useState([]);
+  const getTodos = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/todo");
+      const jsonData = await response.json();
+      setTodos(jsonData);
+      console.log(jsonData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-export default ListTodo
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  return (
+    <>
+      <h1 className="text-center">ListTodo</h1>
+      <table className="table ">
+        <thead>
+          <tr>
+            <th>Todo_id</th>
+            <th>Description</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map((todo) => (
+            <tr key={todo.todo_id}>
+              <td>{todo.todo_id}</td>
+              <td>{todo.description}</td>
+              <td>
+                <button className="btn btn-warning">Edit</button>
+              </td>
+              <td>
+                <button className="btn btn-danger">Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default ListTodo;
