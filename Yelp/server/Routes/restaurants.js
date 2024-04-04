66f3f51066ec89eaed5e2d2a1d41bb704c0da262
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-04-04 22:16:19 +0530

    Implemement the restaurant route with the validation

--- Yelp/server/Routes/restaurants.js
+++ Yelp/server/Routes/restaurants.js
@@ -1,32 +1,63 @@
 const express = require('express');
 const router = express.Router();
+const {checkRestaurantExists} = require('../middleware/validateRestaurant')
 
-router.get('/', (req, res) => {
-    res.status(200).send("Restaurants are fetching ...");
-})
+const pool = require('../db/index')
 
-router.get('/:id', (req, res) => {
-    const { id } = req.params;
-    console.log(req.params)
-    res.status(200).send(`Restaurant with the given id ${id} are fetching ... `);
-})
-
-router.post('/', (req, res) => {
-    console.log('Creating Restaurants...')
-    res.status(200).send(`Restaurant are adding ... `);
+router.get('/', async (req, res) => {
+    try {
+        const results = await pool.query('SELECT * FROM restaurants');
+        res.status(200).send(results.rows);
+    } catch (error) {
+        console.log(error.message);
+    }
 
 })
 
-router.put('/:id', (req, res) => {
-    const { id } = req.params;
-    console.log(`Updating the restaurant with the given id ${id}`);
-    res.status(200).send('Restaurant are updating ...');
+router.get('/:id', checkRestaurantExists,async (req, res) => {
+    try {
+        const { id } = req.params;
+        const results = await pool.query('SELECT * FROM restaurants WHERE id = $1', [id])
+        res.status(200).send(results.rows);
+    } catch (error) {
+        console.log(error.message);
+    }
+
 })
 
-router.delete('/:id', (req, res) => {
-    const { id } = req.params;
-    console.log(`Deleting the Restaurants with the given id ${id}`);
-    res.status(200).send('Restaurant are deleting ...');
+router.post('/', async (req, res) => {
+    try {
+        const { name, location, price_range } = req.body;
+        const addRestaurant = await pool.query('INSERT INTO restaurants (name,location,price_range) VALUES ($1 ,$2, $3)', [name, location, price_range])
+        res.status(200).json({ message: 'Restaurant added successfully' });
+    } catch (error) {
+        console.log(error.message);
+    }
+
+
+})
+
+router.put('/:id',checkRestaurantExists, async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, location, price_range } = req.body;
+        const updateRestaurant = await pool.query('UPDATE restaurants SET name = $1 ,location = $2 ,price_range = $3 WHERE id = $4 ', [name, location, price_range, id])
+        res.status(200).send('Restaurant Updated Successfully .');
+    } catch (error) {
+        console.log(error.message)
+    }
+
+})
+
+router.delete('/:id',checkRestaurantExists, async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleteTodo = await pool.query('DELETE FROM restaurants WHERE id = $1', [id])
+        res.status(200).send('Restaurant deleted Successfully ');
+    } catch (error) {
+        console.log(error.message);
+    }
+
 })
 
 module.exports = router;
