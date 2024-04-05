a392f5d55c01900d349b9306bd650a253f065b6b
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-04-05 22:44:08 +0530

    Implement the Routing of the pages

--- Yelp/client/src/App.jsx
+++ Yelp/client/src/App.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import './App.css'
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import UpdatePage from "./components/UpdatePage";
+import RestaurantDetails from "./components/RestaurantDetails";
 function App() {
-
   return (
     <>
-      <h1 className='text-center text-primary'>Welcome to Yelp</h1>
+      <h1 className="text-center text-primary">Welcome to Yelp</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/restaurants/:id" element={<RestaurantDetails />} />
+          <Route path="/restaurants/:id/update" element={<UpdatePage />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
