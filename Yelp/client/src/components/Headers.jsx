68cc4edf7879e9ef6ffb9d91da2685578d32deaf
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-04-05 23:30:06 +0530

    Created the header component

---
+++ Yelp/client/src/components/Headers.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Headers = () => {
+  return (
+    <>
+      <h1 className="text-center text-primary fw-semibold  mt-4 ">
+        Restaurant Finder
+      </h1>
+    </>
+  );
+};
+
+export default Headers;
