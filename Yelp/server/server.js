103c0dec44fd8fc1b0894940c173ccdbb1005468
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-04-06 15:53:33 +0530

    Add the cors policy for connecting the frontend to the backend

--- Yelp/server/server.js
+++ Yelp/server/server.js
@@ -2,10 +2,16 @@ require('dotenv').config()
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
-
+const cors = require('cors');
 const restaurantRouter = require('./Routes/restaurants');
 
+const corsOptions = {
+    origin: "http://localhost:5173",
+    mehtods: "GET,POST,DELETE,PUT,PATCH,HEAD",
+    creadentials: true
+}
 // Middlewares
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(morgan('dev'));
 
