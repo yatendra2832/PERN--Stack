0c1851fa00b9ef82fbfe2e8ba5c75d4f90e759fe
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-04-09 22:36:43 +0530

    Create the Update page of the restaurant

---
+++ Yelp/client/src/pages/UpdatePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import UpdateRestaurant from "../components/UpdateRestaurant";
+const UpdatePage = () => {
+  return (
+    <>
+      <h1 className="text-center text-primary">Update Restaurant</h1>
+      <UpdateRestaurant/>
+    </>
+  );
+};
+
+export default UpdatePage;
