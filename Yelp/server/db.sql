2224c392a73599ddc3c55cc8e231f65d8134d19f
Yatendra Singh <yatendrayadav2832@gmail.com>
2024-04-12 08:54:06 +0530

    Created table for reviews rating system

--- Yelp/server/db.sql
+++ Yelp/server/db.sql
@@ -5,8 +5,18 @@ CREATE DATABASE yelp;
 \c yelp;
 
 CREATE TABLE restaurants (
-id BIGSERIAL NOT NULL,
+id BIGSERIAL NOT NULL PRIMARY KEY,
 name VARCHAR(50) NOT NULL,
 location VARCHAR(50) NOT NULL,
 price_range INT NOT NULL CHECK( price_range >= 1 AND price_range <= 5 )
+);
+
+-- Review table
+CREATE TABLE reviews (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    restaurant_id BIGINT NOT NULL REFERENCES restaurants(id),
+    name VARCHAR(50) NOT NULL,
+    review TEXT NOT NULL,
+    rating INT NOT NULL CHECK(rating >=1 AND rating <=5)
+
 );
